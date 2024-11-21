b135e3a248f0d1c60f2072bd72e5cbb7391dffd9
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-11-21 16:16:20 +0900

    :sparkles: [feat] user 기능 추가

--- repositorys/userRepository.js
+++ repositorys/userRepository.js
@@ -42,6 +42,44 @@ const getUserById = (id) => {
   });
 };
 
+const deleteUser = (userId, userName) => {
+  return prisma.user.update({
+    where: {
+      id: userId,
+    },
+    data: {
+      userName: userName,
+      nickname: "탈퇴한 회원",
+      password: null,
+      role: "USER",
+      refreshToken: null,
+      provider: null,
+      providerId: null,
+    },
+  });
+};
+
+const updateUserBooth = async (role, boothId, type) => {
+  const booth = await prisma.booth.findFirst({
+    where: { id: boothId },
+  });
+
+  if (!booth) {
+    throw new Error("부스를 찾을 수 없습니다.");
+  }
+
+  await prisma.$transaction(async (pr) => {
+    await pr.user.update({
+      where: { id: booth.userId },
+      data: { role: role },
+    }),
+      await pr.booth.update({
+        where: { id: boothId },
+        data: { accept: type },
+      });
+  });
+};
+
 export default {
   getUserByProvider,
   getUserByUserName,
@@ -49,4 +87,6 @@ export default {
   createProviderUser,
   createNormalUser,
   getUserById,
+  deleteUser,
+  updateUserBooth,
 };
