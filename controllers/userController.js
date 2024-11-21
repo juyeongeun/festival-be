3ad7c41aaca18c5b783b4947f73f16060af69703
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-11-21 17:09:10 +0900

    :sparkles: :bug: [feat&fix] 관리자 로그인 엔드포인트 추가, Router.js 파일 수정

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,106 +4,90 @@ import cookiesConfig from "../config/cookieConfig.js";
 
 const create = asyncHandle(async (req, res, next) => {
   try {
-    const { userName, password, nickname, provider, providerId } = req.body;
+    const { userName, nickname, provider, providerId } = req.body;
 
-    // 1. 소셜 로그인 또는 일반 로그인인지 확인
-    if (provider) {
-      // 소셜 사용자 확인
-      const user = await userService.getProviderMe({ provider, providerId });
-      if (user) {
-        // 로그인
-        const accessToken = userService.createToken(user);
-        const refreshToken = userService.createToken(user, "refresh");
-        const nextUser = await userService.updateUser(user.id, {
-          refreshToken,
-        });
+    // 소셜 사용자 확인
+    const user = await userService.getProviderMe({ provider, providerId });
+    if (user) {
+      // 로그인
+      const accessToken = userService.createToken(user);
+      const refreshToken = userService.createToken(user, "refresh");
+      const nextUser = await userService.updateUser(user.id, {
+        refreshToken,
+      });
 
-        res.cookie(
-          "access-token",
-          accessToken,
-          cookiesConfig.accessTokenOption
-        );
-        res.cookie(
-          "refresh-token",
-          refreshToken,
-          cookiesConfig.refreshTokenOption
-        );
+      res.cookie("access-token", accessToken, cookiesConfig.accessTokenOption);
+      res.cookie(
+        "refresh-token",
+        refreshToken,
+        cookiesConfig.refreshTokenOption
+      );
 
-        res.status(200).send(nextUser);
-      } else {
-        // 회원가입
-        const refreshToken = userService.createToken({}, "refresh");
-        const newUser = await userService.createProviderUser({
-          userName,
-          nickname,
-          provider,
-          providerId,
-          refreshToken,
-        });
-        const accessToken = userService.createToken(newUser);
-
-        res.cookie(
-          "access-token",
-          accessToken,
-          cookiesConfig.accessTokenOption
-        );
-        res.cookie(
-          "refresh-token",
-          refreshToken,
-          cookiesConfig.refreshTokenOption
-        );
-
-        res.status(201).send(newUser);
-      }
+      res.status(200).send(nextUser);
     } else {
-      // 일반 로그인
-      const user = await userService.getNormalMe({ userName, password });
-      if (user) {
-        // 로그인
-        const accessToken = userService.createToken(user);
-        const refreshToken = userService.createToken(user, "refresh");
-        const nextUser = await userService.updateUser(user.id, {
-          refreshToken,
-        });
+      // 회원가입
+      const refreshToken = userService.createToken({}, "refresh");
+      const newUser = await userService.createProviderUser({
+        userName,
+        nickname,
+        provider,
+        providerId,
+        refreshToken,
+      });
+      const accessToken = userService.createToken(newUser);
 
-        res.cookie(
-          "access-token",
-          accessToken,
-          cookiesConfig.accessTokenOption
-        );
-        res.cookie(
-          "refresh-token",
-          refreshToken,
-          cookiesConfig.refreshTokenOption
-        );
+      res.cookie("access-token", accessToken, cookiesConfig.accessTokenOption);
+      res.cookie(
+        "refresh-token",
+        refreshToken,
+        cookiesConfig.refreshTokenOption
+      );
 
-        res.status(200).send(nextUser);
-      } else {
-        // 회원가입
-        const refreshToken = userService.createToken({}, "refresh");
-        const newUser = await userService.createNormalUser({
-          userName,
-          password,
-          nickname,
-          refreshToken,
-        });
-
-        const accessToken = userService.createToken(newUser);
-
-        res.cookie(
-          "access-token",
-          accessToken,
-          cookiesConfig.accessTokenOption
-        );
-        res.cookie(
-          "refresh-token",
-          refreshToken,
-          cookiesConfig.refreshTokenOption
-        );
-
-        res.status(201).send(newUser);
-      }
+      res.status(201).send(newUser);
     }
+  } catch (error) {
+    next(error);
+  }
+});
+
+const loginAdmin = asyncHandle(async (req, res, next) => {
+  try {
+    const { festivalCode, password } = req.body;
+    const user = await userService.getNormalMe({
+      userName: festivalCode,
+      password,
+    });
+    const accessToken = userService.createToken(user);
+    const refreshToken = userService.createToken(user, "refresh");
+    const nextUser = await userService.updateUser(user.id, {
+      refreshToken,
+    });
+
+    res.cookie("access-token", accessToken, cookiesConfig.accessTokenOption);
+    res.cookie("refresh-token", refreshToken, cookiesConfig.refreshTokenOption);
+
+    res.status(200).send(nextUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+const signupAdmin = asyncHandle(async (req, res, next) => {
+  try {
+    const { festivalCode, password } = req.body;
+    const refreshToken = userService.createToken({}, "refresh");
+    const newUser = await userService.createNormalUser({
+      userName: festivalCode,
+      password,
+      refreshToken,
+    });
+
+    const accessToken = userService.createToken(newUser);
+
+    res.cookie("access-token", accessToken, cookiesConfig.accessTokenOption);
+    res.cookie("refresh-token", refreshToken, cookiesConfig.refreshTokenOption);
+
+    res.status(201).send(newUser);
   } catch (error) {
     next(error);
   }
@@ -227,4 +211,6 @@ export default {
   refreshToken,
   getMe,
   deleteMe,
+  loginAdmin,
+  signupAdmin,
 };
