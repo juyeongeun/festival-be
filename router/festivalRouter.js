35295740963a3d2a97757745b019610c9c4d3a61
minsu <a2238737@gmail.com>
2024-11-21 17:39:06 +0900

    :bug: [Fix] 테스트 파일 삭제

--- router/festivalRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import patchFestivalController from "../controllers/festivalControllers.js";
-
-const router = express.Router();
-
-router.route("/:festivalId").patch(patchFestivalController.patchFestival);
-
-export default router;
