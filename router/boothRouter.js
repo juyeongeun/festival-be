38cc5ba119760b18fe92ff9b2f1112cc003b0902
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-11-22 11:08:35 +0900

    :sparkles: [feat] 부스 유효성 검사 기능

--- router/boothRouter.js
+++ router/boothRouter.js
@@ -1,12 +1,16 @@
 import express from "express";
 import passport from "../config/passportConfig.js";
 import boothController from "../controllers/boothController.js";
+import boothValidation from "../middleware/booth/boothValidation.js";
 
 const router = express.Router();
 
 router
   .route("/:festivalId/booth")
-  .all(passport.authenticate("access-token", { session: false }))
+  .all(
+    passport.authenticate("access-token", { session: false }),
+    boothValidation
+  )
   .post(boothController.createBooth);
 
 export default router;
