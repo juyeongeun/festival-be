d289d1e696fb1b96d2717b17bac2d93155e35cd6
minsu <a2238737@gmail.com>
2024-11-24 21:03:50 +0900

    :bug: [Fix] 파일명 수정

--- services/boardServices.js
+++ services/boardServices.js
@@ -1,4 +1,4 @@
-import * as boardRepository from "../repositorys/boardRepositorys.js";
+import * as boardRepository from "../repositorys/boardRepository.js";
 import * as participationRepository from "../repositorys/participationRepository.js";
 const deleteBoard = async (userId, festivalId, boardId) => {
   const festivalUser = await participationRepository.participationCheck(
