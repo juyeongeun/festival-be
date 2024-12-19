2f85a44746f35fad9cf2ba282ac655513af98ec4
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-12-19 15:14:49 +0900

    :sparkles: [feat] socket.io 실시간 알림

--- app.js
+++ app.js
@@ -14,13 +14,46 @@ import payRouter from "./router/payRouter.js";
 import notificationRouter from "./router/notificationRouter.js";
 import reviewRouter from "./router/reviewRouter.js";
 import menuRouter from "./router/menuRouter.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 dotenv.config();
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("클라이언트 연결됨");
+
+  // 사용자 인증 처리
+  socket.on("authenticate", (userId) => {
+    socket.join(`user_${userId}`); // 사용자별 room 생성
+  });
+});
+
+export const sendNotification = (userId, notification) => {
+  io.to(`user_${userId}`).emit("new_notification", notification);
+};
+
+app.post("/test-notification", (req, res) => {
+  const testNotification = {
+    message: "테스트 알림입니다",
+    timestamp: new Date(),
+  };
+
+  // 특정 사용자에게 알림 전송
+  io.to("user_123").emit("new_notification", testNotification);
+
+  res.json({ message: "테스트 알림이 전송되었습니다." });
+});
+
 app.use(express.json());
-//CORS 설정
 const allowedOrigins = ["http://localhost:3000"];
-// CORS 설정
 const corsOptions = {
   credentials: true,
   origin: function (origin, callback) {
@@ -50,4 +83,4 @@ app.use("/menu", menuRouter);
 
 app.use(errorHandler);
 
-app.listen(3001, () => console.log("http://localhost:3001"));
+httpServer.listen(3001);
